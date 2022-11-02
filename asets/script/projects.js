415c0f59ab49e9e63f610f729d5ae5768165b843
K-KawanSousa <108997278+K-KawanSousa@users.noreply.github.com>
2022-11-02 16:09:46 -0300

    gallery interactive quase pronta

--- asets/script/projects.js
+++ asets/script/projects.js
@@ -1,34 +1,84 @@
 var
+gallery = document.querySelector('#projects #gallery'),
 wrapperList = document.querySelectorAll('.wrapper-cover'),
 coverList = document.querySelectorAll('.cover');
 
+    console.log(coverList[0])
+
 wrapperList.forEach((el, index)=>{
     let
-    width = parseFloat(getComputedStyle(el).width),
-    left = el.offsetLeft;
+    galleryWidth = gallery.offsetWidth,
+    wrapperWidth = el.offsetWidth,
+    wrapperLeft = el.offsetLeft,
+    wrapperRight = galleryWidth - wrapperLeft - wrapperWidth;
 
     el.addEventListener('mouseenter', onEnter);
     el.addEventListener('mouseout', onOut);
 
- 
-    if(left < width){
-        el.setAttribute('position', 'left')
+    if(wrapperLeft < wrapperWidth){
+        el.setAttribute('position', 'left');
+        coverList[index].classList.add('cover-lft');
     }
-    else if(left > width){
-        el.setAttribute('position', 'right')
+    else if(wrapperRight < 0){
+        el.setAttribute('position', 'right');
+        coverList[index].classList.add('cover-rgt');
     }
     else{
-        console.log(left, typeof left, typeof width, width)
-        el.setAttribute('position', 'betwen')
+        el.setAttribute('position', 'between');
+        coverList[index].classList.add('cover-btw');
     }
+        console.log(el)
 })
 
 function onEnter(e){
-    e.currentTarget.classList.add('wrapper-cover-hover');
-    e.currentTarget.querySelector('.cover').classList.add('cover-hover');
+    let 
+    element = e.currentTarget,
+    coverEl = element.querySelector('.cover'),
+    position = element.getAttribute('position');
+
+    if(!element.hasAttribute('coming-soon')){
+        switch(position){
+            case 'left':
+                element.classList.add('wppr-cvr-lft-hvr');
+                coverEl.classList.add('cover-hover');
+                break;
+            case 'between':
+                element.classList.add('wppr-cvr-btw-hvr');
+                coverEl.classList.add('cover-hover');
+                break;
+            case 'right':
+                element.classList.add('wppr-cvr-rgt-hvr');
+                coverEl.classList.add('cover-hover');
+                break
+            default: return;
+        }
+    }
+    else{
+    }
+
 }
 
 function onOut(e){
-    e.currentTarget.classList.remove('wrapper-cover-hover');
-    e.currentTarget.querySelector('.cover').classList.remove('cover-hover');
+    let 
+    element = e.currentTarget,
+    coverEl = element.querySelector('.cover'),
+    position = element.getAttribute('position');
+
+    if(!element.hasAttribute('coming-soon')){
+        switch(position){
+            case 'left':
+                element.classList.remove('wppr-cvr-lft-hvr');
+                coverEl.classList.remove('cover-hover');
+                break;
+            case 'between':
+                element.classList.remove('wppr-cvr-btw-hvr');
+                coverEl.classList.remove('cover-hover');
+                break;
+            case 'right':
+                element.classList.remove('wppr-cvr-rgt-hvr');
+                coverEl.classList.remove('cover-hover');
+                break
+            default: return;
+        }
+    }
 }
