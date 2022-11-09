3a7821c6b4137826541524c2984d485a165f3dbf
K-KawanSousa <108997278+K-KawanSousa@users.noreply.github.com>
2022-11-09 16:25:10 -0300

    form right

--- asets/script/contact.js
+++ asets/script/contact.js
@@ -1,3 +1,17 @@
-var 
-inputLst = document.querySelectorAll('.contact .field-inpt'),
-edgeClassLst = [];
+var
+itemWrapperLst = document.querySelectorAll('.form-item');
+
+inputLst = document.querySelectorAll('.form-npt');
+
+inputLst.splice(0, inputLst.length, )
+console.log(inputLst.Node.removeChild);
+
+inputLst.forEach(()=>{
+    addEventListener('click', inptFocus)
+})
+
+function inptFocus(){
+    console.log('ok');
+}
+
+// console.log();
