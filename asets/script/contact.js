b9d40137815f8e9526940d4d71cd700a081b30ad
K-KawanSousa <108997278+K-KawanSousa@users.noreply.github.com>
2022-11-10 00:09:31 -0300

    interativo contact-me

--- asets/script/contact.js
+++ asets/script/contact.js
@@ -1,17 +1,56 @@
 var
-itemWrapperLst = document.querySelectorAll('.form-item');
+forItemList = [].slice.call(document.querySelectorAll('.form-item')),
+inputLst = [].slice.call(document.querySelectorAll('.form-npt'));
 
-inputLst = document.querySelectorAll('.form-npt');
+inputLst.pop()
+forItemList.pop()
 
-inputLst.splice(0, inputLst.length, )
-console.log(inputLst.Node.removeChild);
+inputLst.forEach((el, index) => {
+    
+    addEventListener('focusin', inFocus);
+    addEventListener('focusout', outFocus);
+});
 
-inputLst.forEach(()=>{
-    addEventListener('click', inptFocus)
-})
+function inFocus(e){
+    if(e.target.value.length > 0) return;
 
-function inptFocus(){
-    console.log('ok');
+    let formField = e.target.closest('.form-field');
+    
+    formField.classList.add('focus');
 }
 
-// console.log();
+function outFocus(e){
+    if(e.target.value.length > 0) return;
+    let formField = e.target.closest('.form-field');
+    formField.classList.remove('focus');
+}
+
+// form validator
+formValidator()
+
+function formValidator(){
+
+    inputLst[0].addEventListener('input', onInput);
+    inputLst[1].addEventListener('input', onInput);
+}
+
+function onInput(e){
+    let
+    regEx = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}'),
+    el = e.target;
+    if(regEx.test(el.textContent)){
+        console.log('ok');
+    }
+
+    if(el.id == 'npt-name'){
+        tstName()
+    }
+}
+
+function tstName(){
+    this.regEx = new RegExp('[a-z,A-z]');
+
+    console.log(regEx.test(el))
+}
+
+// console.log(inputLst)
