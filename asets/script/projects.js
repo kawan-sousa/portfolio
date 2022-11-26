c1bf318da874f771eaa76cffec42925e4981db02
K-KawanSousa <108997278+K-KawanSousa@users.noreply.github.com>
2022-11-26 15:58:03 -0300

    gallery hover's ready

--- asets/script/projects.js
+++ asets/script/projects.js
@@ -1,89 +1,3 @@
-// var
-// gallery = document.querySelector('#projects #gallery'),
-// wrapperList = document.querySelectorAll('.wrapper-cover'),
-// coverList = document.querySelectorAll('.cover');
-
-
-// wrapperList.forEach((el, index)=>{
-//     if(el.hasAttribute('coming-soon')) return
-//     let
-//     galleryWidth = gallery.offsetWidth,
-//     wrapperWidth = el.offsetWidth,
-//     wrapperLeft = el.offsetLeft,
-//     wrapperRight = galleryWidth - wrapperLeft - wrapperWidth;
-
-//     el.addEventListener('mouseenter', onEnter);
-//     el.addEventListener('mouseout', onOut);
-
-//     if(wrapperLeft < wrapperWidth){
-//         el.setAttribute('position', 'left');
-//         coverList[index].classList.add('cover-lft');
-//     }
-//     else if(wrapperRight < 0){
-//         el.setAttribute('position', 'right');
-//         coverList[index].classList.add('cover-rgt');
-//     }
-//     else{
-//         el.setAttribute('position', 'between');
-//         coverList[index].classList.add('cover-btw');
-//     }
-// })
-
-// function onEnter(e){
-//     let 
-//     element = e.currentTarget,
-//     coverEl = element.querySelector('.cover'),
-//     position = element.getAttribute('position');
-
-//     if(!element.hasAttribute('coming-soon')){
-//         switch(position){
-//             case 'left':
-//                 element.classList.add('wppr-cvr-lft-hvr');
-//                 coverEl.classList.add('cover-hover');
-//                 break;
-//             case 'between':
-//                 element.classList.add('wppr-cvr-btw-hvr');
-//                 coverEl.classList.add('cover-hover');
-//                 break;
-//             case 'right':
-//                 element.classList.add('wppr-cvr-rgt-hvr');
-//                 coverEl.classList.add('cover-hover');
-//                 break
-//             default: return;
-//         }
-//     }
-//     else{
-//     }
-
-// }
-
-// function onOut(e){
-//     let 
-//     element = e.currentTarget,
-//     coverEl = element.querySelector('.cover'),
-//     position = element.getAttribute('position');
-
-//     coverEl.style = 'z-index: 1;';
-//     setTimeout(()=>{coverEl.style = ''}, '500')
-//     if(!element.hasAttribute('coming-soon')){
-//         switch(position){
-//             case 'left':
-//                 element.classList.remove('wppr-cvr-lft-hvr');
-//                 coverEl.classList.remove('cover-hover');
-//                 break;
-//             case 'between':
-//                 element.classList.remove('wppr-cvr-btw-hvr');
-//                 coverEl.classList.remove('cover-hover');
-//                 break;
-//             case 'right':
-//                 element.classList.remove('wppr-cvr-rgt-hvr');
-//                 coverEl.classList.remove('cover-hover');
-//                 break
-//             default: return;
-//         }
-//     }
-// }
-
 var
 gallery = document.querySelector('#projects #gallery'),
 innerWpprList = document.querySelectorAll('.wrapper-cover'),
@@ -115,20 +29,28 @@ function onEnter(e){
 }
 
 function onOut(){
-    let transitionDeleyStrg = window.getComputedStyle(currentWpprEl).getPropertyValue('transition-delay'),
-    transitionDeleyNbr = parseFloat(transitionDeleyStrg.match(/\d/g).join(''))
+    const transitionDeleyStrg = window.getComputedStyle(currentWpprEl).getPropertyValue('transition-delay'),
+    wrapperOut = currentWpprEl.closest('.wrapper-out');
 
     switch(checkPosition()){
         case 'left':
             currentWpprEl.classList.remove('hover');
-            currentWpprEl.style = 'z-index: 1; overflow: visible;'
-            setTimeout(()=>{currentWpprEl.style = '';}, `${transitionDeleyNbr}00`)
+            wrapperOut.style = 'z-index: 1;';
+            setTimeout(()=>{wrapperOut.style = '';}, `1700`);
             break;
         case 'between':
             currentWpprEl.classList.remove('hover-btw');
+            wrapperOut.style = 'z-index: 1;';
+            setTimeout(()=>{wrapperOut.style = '';}, `1700`);
             break;
         default:
             currentWpprEl.classList.remove('hover-rgt');
+            wrapperOut.style = 'z-index: 1;';
+            currentWpprEl.style = 'left: initial;right: 0;';
+            setTimeout(()=>{
+                wrapperOut.style = '';
+                currentWpprEl.style = ''
+                }, `1700`);
             break;
     }
 }
