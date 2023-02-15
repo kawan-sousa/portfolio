e91b937856eba5a08ea01d38f262c57ed97d1d1e
kawan-sousa <kawansousa.dev@gmail.com>
2023-02-15 12:15:29 -0300

    implemented: breack moviments

--- asets/script/parallax.js
+++ asets/script/parallax.js
@@ -1,50 +1,54 @@
-const paralaxNdList = document.querySelectorAll('.paralax');
-const paralaxElList = objectArray(paralaxNdList);
-const sensibility = .4; //set the sensibility of paralax efect
+const paralaxList = document.querySelectorAll('.paralax');
 
-setDefaltAttr(paralaxElList);
-
-// turns a NodeList in a object array and return this
-function objectArray(nodeList){
-    const array = Array.from(nodeList); // turns the nodeList into a array
-    let objectArray =[];
-
-    array.forEach((item, i)=>{
-        objectArray.push({element: item});
-    })
-    return objectArray;
-}
-
-function setDefaltAttr(arrayObject){
-    arrayObject.forEach(el=>{
-        el.translateY = 0
-        el.lastClientTop = paralaxElList[0].element.getBoundingClientRect().top;// set element client top
-    })
-}
-
-window.addEventListener('scroll', ()=>{
-        paralaxElList.forEach((plxEl)=>{
-            if(getOffsetBottom(plxEl.element) < 0){
-                plxEl.element.style.transform = 'translateY(0px)'
-            };
-            const verticalMov = calcVerticalMov(plxEl)
-            plxEl.element.style.transform = `translateY(-${verticalMov * sensibility}px)`;
-        })
+//initialize the attributes
+paralaxList.forEach(obj=>{
+    obj.sensibility = obj.getAttribute('plx-sensi');
+    obj.translateY = 0;
+    obj.minTranslateY = -getExposedY(obj);
+    obj.maxTranslateY = 0;
+    obj.lastClientTop= obj.getBoundingClientRect().top;
 })
 
-function calcVerticalMov(plxEl){
-    const currentCltTop = plxEl.element.getBoundingClientRect().top;
+window.addEventListener('scroll', onScroll)
 
-    return plxEl.lastClientTop - currentCltTop;
+function onScroll(){
+    paralaxList.forEach(obj=>{
+        const translateY = -getMovY(obj);
+
+        if(checkInClient(obj)) obj.style.transform = '';
+        if(translateY > obj.maxTranslateY){
+            obj.style.transform = `translateY(${obj.maxTranslateY}px)`;
+            console.log('maxTranslateY');
+            obj.translateY = translateY;
+            return;
+        }
+        if(translateY < obj.minTranslateY){
+            obj.style.transform = `translateY(${obj.minTranslateY}px)`;
+            console.log('minTranslateY');
+            obj.translateY = translateY;
+            return;
+        }
+        obj.style.transform = `translateY(${translateY}px)`; //por que só funciona com o sinal de "-1"
+        obj.translateY = translateY;
+    })
 }
 
-function getOffsetBottom(el){
-    const parentEl = el.offsetParent;
+function getMovY(obj){
+    return obj.sensibility *(obj.lastClientTop - obj.getBoundingClientRect().top);
+}
 
-    // get transformY of element
-    const transform = window.getComputedStyle(el).transform;
-    const translateY = new DOMMatrix(transform).m42;
+function getExposedY(obj){
+    const parentHeight = obj.offsetParent.offsetHeight;
+    const objHeight = obj.offsetHeight;
 
-    const bottom = el.offsetHeight - (parentEl.offsetHeight + translateY * -1)
-    return bottom
+    return objHeight - parentHeight;
+}
+
+function checkInClient(obj){
+    const clientTop = obj.getBoundingClientRect().top;
+    const objHeight = obj.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const isShowing = clientTop < windowHeight && clientTop > -objHeight;
+
+    return isShowing
 }
